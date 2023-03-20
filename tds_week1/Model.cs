bc3192c1bcfe666c036ec28e5de709fc924f9f5c
Isadora <ysoca98@hotmail.com>
2023-03-19 23:33:23 -0300

    first commit

---
+++ tds_week1/Model.cs
@@ -0,0 +1,12 @@
+﻿namespace tds_week1
+{
+    public class Model
+    {
+        
+	public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string? Description { get; set; }
+        public bool Done { get; set; }
+    }
+}
+
